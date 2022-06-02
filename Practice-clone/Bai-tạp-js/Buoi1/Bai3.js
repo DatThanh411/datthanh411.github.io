5d9d8466b7537af02f25ecd53d8a13e2aee9f754
DatThanh411 <datthanh4111@gmail.com>
2022-06-02 17:16:42 +0700

    update

--- Practice-clone/Bai-tạp-js/Buoi1/Bai3.js
+++ Practice-clone/Bai-tạp-js/Buoi1/Bai3.js
@@ -4,8 +4,6 @@ var x = document.querySelectorAll("li:first-child");
 
 var y = document.querySelectorAll("li:last-child");
 
-const z = document.createElement('li');
-
 
 for(let i = 0; i < x.length; i++) {
     x[i].innerHTML = "First";
